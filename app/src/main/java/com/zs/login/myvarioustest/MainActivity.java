e732731986c38b0b645e025061d1932b62f0eb91
CodeZhang <552146383@qq.com>
2017-05-10 15:30:21 +0800

    我的test

---
+++ app/src/main/java/com/zs/login/myvarioustest/MainActivity.java
@@ -0,0 +1,70 @@
+package com.zs.login.myvarioustest;
+
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
+
+import com.zs.login.myvarioustest.view.AnimButton;
+import com.zs.login.myvarioustest.view.RadarView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Bind(R.id.btn)
+    AnimButton button;
+    @Bind(R.id.radarview)
+    RadarView radarView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        if (Build.VERSION.SDK_INT >= 21) {
+            View decorView = getWindow().getDecorView();
+            int option = View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
+            decorView.setSystemUiVisibility(option);
+            getWindow().setNavigationBarColor(Color.TRANSPARENT);
+            getWindow().setStatusBarColor(Color.TRANSPARENT);
+        }
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+
+        button.setAnimationButtonListener(new AnimButton.AnimationButtonListener() {
+            @Override
+            public void onClickListener() {
+                button.startAnim();
+            }
+
+            @Override
+            public void animationFinish() {
+                Toast.makeText(MainActivity.this,"animationFinish",Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+//    @Override
+//    public void onWindowFocusChanged(boolean hasFocus) {
+//        super.onWindowFocusChanged(hasFocus);
+//        if (hasFocus && Build.VERSION.SDK_INT >= 19) {
+//            View decorView = getWindow().getDecorView();
+//            decorView.setSystemUiVisibility(
+//                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+//                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+//                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+//                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+//                            | View.SYSTEM_UI_FLAG_FULLSCREEN
+//                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+//        }
+//    }
+
+}
---
+++ app/src/main/java/com/zs/login/myvarioustest/view/RadarView.java
@@ -0,0 +1,193 @@
+package com.zs.login.myvarioustest.view;
+
+import android.animation.Animator;
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+import android.view.animation.DecelerateInterpolator;
+
+/**
+ * Created by zs
+ * Date：2017年 05月 09日
+ * Time：20:17
+ * —————————————————————————————————————
+ * About: 雷达扫描效果
+ * —————————————————————————————————————
+ */
+
+public class RadarView extends View {
+
+    private Paint mBgPaint;
+
+    private Paint mCriclePaint;
+
+    private int mRadius;
+
+    private int mCricleRadius;
+
+    private ValueAnimator[] mValueAnimators = new ValueAnimator[2];
+
+    private AnimatorSet mAnimatorSet;
+
+    private AnimatorSet mAnimatorScaleSet;
+
+    private int mColor = 0xFF4081;
+
+    public RadarView(Context context) {
+        this(context,null);
+    }
+
+    public RadarView(Context context, @Nullable AttributeSet attrs) {
+        this(context, attrs,0);
+    }
+
+    public RadarView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+
+        initPaint();
+        initAnimation();
+        initScaleAnimSet();
+        initAnimatorSet();
+
+    }
+
+    /**
+     * 初始化画笔
+     */
+    private void initPaint(){
+
+        mBgPaint = new Paint();
+        mBgPaint.setAntiAlias(true);
+        mBgPaint.setColor(0x66000000);
+        mBgPaint.setStyle(Paint.Style.FILL);
+
+        mCriclePaint = new Paint();
+        mCriclePaint.setAntiAlias(true);
+        mCriclePaint.setColor(mColor);
+        mCriclePaint.setStrokeWidth(3);
+        mCriclePaint.setStyle(Paint.Style.STROKE);
+
+    }
+
+    /**
+     * 初始化动画
+     */
+    private void initAnimation(){
+
+        for(int i = 0 ; i < mValueAnimators.length;i++ ){
+            mValueAnimators[i] = new ValueAnimator().ofFloat(0,1);
+            mValueAnimators[i].setDuration(2000);
+            mValueAnimators[i].setInterpolator(new DecelerateInterpolator());
+            mValueAnimators[i].addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                @Override
+                public void onAnimationUpdate(ValueAnimator animation) {
+                    float value = (float) animation.getAnimatedValue();
+                    mCricleRadius = (int) (value * mRadius);
+                    if (value > 0.7){
+                        mCriclePaint.setAlpha((int) (255 * (1-value)));
+                    }else{
+                        mCriclePaint.setAlpha(100);
+                    }
+                    mCriclePaint.setStrokeWidth(3 + value);
+                    invalidate();
+                }
+            });
+        }
+    }
+
+    private void initScaleAnimSet() {
+        //沿x轴放大
+        ObjectAnimator scaleXAnimator = ObjectAnimator.ofFloat(this, "scaleX", 1f, 1.2f, 1f);
+        //沿y轴放大
+        ObjectAnimator scaleYAnimator = ObjectAnimator.ofFloat(this, "scaleY", 1f, 1.2f, 1f);
+
+        //沿x轴放大
+        ObjectAnimator scaleXAnimator2 = ObjectAnimator.ofFloat(this, "scaleX", 1f, 1.2f, 1f);
+        //沿y轴放大
+        ObjectAnimator scaleYAnimator2 = ObjectAnimator.ofFloat(this, "scaleY", 1f, 1.2f, 1f);
+        mAnimatorScaleSet = new AnimatorSet();
+
+        //同时沿X,Y轴放大
+        mAnimatorScaleSet.play(scaleXAnimator).with(scaleYAnimator);
+        mAnimatorScaleSet.play(scaleXAnimator2).with(scaleYAnimator2).after(scaleXAnimator);
+        //都设置2s，也可以为每个单独设置
+        mAnimatorScaleSet.setDuration(2000);
+        mAnimatorScaleSet.start();
+
+        mAnimatorScaleSet.addListener(new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                mAnimatorScaleSet.start();
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animation) {
+
+            }
+        });
+
+    }
+
+    /**
+     * 初始化动画组
+     */
+    private void initAnimatorSet(){
+        mAnimatorSet = new AnimatorSet();
+        mAnimatorSet.play(mValueAnimators[0]).before(mValueAnimators[1]);
+        mAnimatorSet.start();
+        mAnimatorSet.addListener(new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                mAnimatorSet.start();
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animation) {
+
+            }
+        });
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        mRadius = (int) (Math.min(w,h) * 0.3);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        // 画圆背景
+        canvas.drawCircle(getWidth()/2,getHeight()/2,mRadius,mBgPaint);
+        // 画圆环
+        canvas.drawCircle(getWidth()/2,getHeight()/2,mCricleRadius,mCriclePaint);
+    }
+
+}
