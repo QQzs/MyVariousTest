de2a6123411f8098aeb11ff9010a3fc643ded5f1
CodeZhang <552146383@qq.com>
2019-07-17 11:42:34 +0800

    shadow

--- app/src/main/java/com/zs/various/span/CenterAlignImageSpan.java
+++ app/src/main/java/com/zs/various/span/CenterAlignImageSpan.java
@@ -1,6 +1,5 @@
 package com.zs.various.span;
 
-import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.drawable.Drawable;
@@ -22,19 +21,17 @@ public class CenterAlignImageSpan extends ImageSpan {
 
     }
 
-    public CenterAlignImageSpan(Bitmap b) {
-        super(b);
-    }
-
     @Override
     public void draw(@NonNull Canvas canvas, CharSequence text, int start, int end, float x, int top, int y, int bottom,
                      @NonNull Paint paint) {
 
         Drawable b = getDrawable();
         Paint.FontMetricsInt fm = paint.getFontMetricsInt();
-        int transY = (y + fm.descent + y + fm.ascent) / 2 - b.getBounds().bottom / 2;//计算y方向的位移
+        //计算y方向的位移
+        int transY = (y + fm.descent + y + fm.ascent) / 2 - b.getBounds().bottom / 2;
         canvas.save();
-        canvas.translate(x, transY);//绘制图片位移一段距离
+        //绘制图片位移一段距离
+        canvas.translate(x, transY);
         b.draw(canvas);
         canvas.restore();
     }
