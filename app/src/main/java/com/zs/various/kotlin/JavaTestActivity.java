04baa31d54158ba9ace6b5d991001a4ccb9dbcb8
nengmao@uxin.com <nengmao@uxin.com>
2021-12-24 14:31:41 +0800

    [comments]： kotlin 升级 [jira_id]：nengmao [version]：

--- app/src/main/java/com/zs/various/kotlin/JavaTestActivity.java
+++ app/src/main/java/com/zs/various/kotlin/JavaTestActivity.java
@@ -1,15 +1,21 @@
 package com.zs.various.kotlin;
 
 import android.annotation.SuppressLint;
-import android.media.Image;
+import android.os.Build;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.annotation.RequiresApi;
+
 import com.zs.various.R;
 import com.zs.various.base.BaseActivity;
 import com.zs.various.kotlin.extension.KotlinExtensionKt;
+import com.zs.various.kotlin.view.TestView;
+
+import kotlin.Unit;
+import kotlin.jvm.functions.Function2;
 
 /**
  * @Author: zs
@@ -21,6 +27,8 @@ public class JavaTestActivity extends BaseActivity {
     private TextView tv;
     private ImageView iv;
 
+    private TestView testView;
+
     @Override
     protected int setLayoutId() {
         return R.layout.kotlin_test_layout;
@@ -31,15 +39,17 @@ public class JavaTestActivity extends BaseActivity {
 
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.N)
     @SuppressLint("ClickableViewAccessibility")
     @Override
     public void initData() {
 
         tv = findViewById(R.id.tv_age);
         iv = findViewById(R.id.iv_avatar);
+        testView = findViewById(R.id.test_view);
 
-        KotlinTestActivity.Companion.getNum4();
-
+        KotlinTestActivity.getNum4();
+        KotlinTestActivity.Companion.getData();
         KotlinTestActivity.getNewData();
 
         int b = KotlinTestActivity.StaticData.num7;
@@ -54,6 +64,17 @@ public class JavaTestActivity extends BaseActivity {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
                 return false;
+            }
+        });
+
+
+        // java 调用Kotlin回调函数
+        testView.setCallBack(new Function2<String, Integer, Unit>() {
+
+            @Override
+            public Unit invoke(String s, Integer integer) {
+
+                return null;
             }
         });
 
